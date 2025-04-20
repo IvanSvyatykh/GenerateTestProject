cbffbd2662cb50df8993d23bfcf2ab51e37a2840
Insignificance <happywin855@gmail.com>
2025-04-20 22:43:43 +0500

    fix: Исправлена генерация файлов, а так же промт. Теперь при формате ответов - "Варианты ответа" ответы на вопросы приходят не в индексах. Изменены варианты ответов с "-" на индексы.

--- telegramm_bot/app/handlers/file_format_hanlder.py
+++ telegramm_bot/app/handlers/file_format_hanlder.py
@@ -43,7 +43,6 @@ async def file_format_handler(callback_query: CallbackQuery, state: FSMContext):
 
     with open(file_path, "rb"):
         document = FSInputFile(file_path, filename=os.path.basename(file_path))
-    """
     print("Log to db")
     async with get_async_session() as db_session:
         print("start logging...")
@@ -63,7 +62,6 @@ async def file_format_handler(callback_query: CallbackQuery, state: FSMContext):
         }
         await logs_repo.add_log(log)
         print("end logging ...")
-        """
     await callback_query.message.answer_document(
         document=document,
         caption=f"Тест в формате {file_format.upper()}",
